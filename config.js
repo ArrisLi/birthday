ec8af7b4f58910850be69e5c95bb318657879709
ArrisLi <arieslee1@hotmail.com>
2022-01-04 23:40:22 +1300

    Update config.js

--- config.js
+++ config.js
@@ -34,7 +34,7 @@ var config = {
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
-        turn_on: "点我点我！",
+        turn_on: "->点我点我!",
         play: "音乐走起～",
         bannar_coming: "加点装饰",
         balloons_flying: "还少点东西？",
